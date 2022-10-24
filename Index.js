24dae27c0179749c52a5b60f5afd04cc0226af78
BelialDaNoS <lautarogazzo1@gmail.com>
2022-10-24 17:12:03 -0300

    ENTREGA 1 - Clase 2. Principios básicos de Javascript

--- Index.js
+++ Index.js
@@ -6,6 +6,46 @@
 
 // mostrarLista(datos);
 
-(function(lista = [123,456,678]){
-    (lista.length < 1) ? console.log("Lista vacía") : console.table(lista)
-})();
+// (function(lista = [123,456,678]){
+//     (lista.length < 1) ? console.log("Lista vacía") : console.table(lista)
+// })();
+
+
+class Contador {
+
+    static contadorGlobal = 0;
+
+    constructor(nombre){
+        this.nombre = nombre;
+        this.contadorIndividual = 0;
+    }
+
+    obtenerResponsable (){
+        console.log(`El responsable es ${this.nombre}`)
+    }
+
+    obtenerCuentaIndividual(){
+        console.log(`La cuenta individual de ${this.nombre} es de ${this.contadorIndividual} `)
+    }
+
+    obtenerCuentaGlobal(){
+        console.log(`Cuenta Global: ${this.constructor.contadorGlobal}`)
+    }
+
+    contar(){
+        this.contadorIndividual++
+        this.constructor.contadorGlobal++
+    }
+}
+
+const contador1 = new Contador("Pablo");
+const contador2 = new Contador("Lauty");
+
+contador1.contar();
+contador2.contar();
+
+console.log(`${contador1.nombre} : ${contador1.contadorIndividual}`);
+
+console.log(`${contador2.nombre} : ${contador2.contadorIndividual}`);
+
+contador1.obtenerCuentaGlobal();
