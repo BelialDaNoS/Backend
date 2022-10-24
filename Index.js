7103c125e334c82d1e643c6e6ca2e0749052cf1d
BelialDaNoS <lautarogazzo1@gmail.com>
2022-10-24 10:24:12 -0300

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,11 @@
+
+
+// function mostrarLista(lista){
+//     (lista.length < 1) ? console.log("Lista vacía") : console.table(lista)
+// }
+
+// mostrarLista(datos);
+
+(function(lista = [123,456,678]){
+    (lista.length < 1) ? console.log("Lista vacía") : console.table(lista)
+})();
